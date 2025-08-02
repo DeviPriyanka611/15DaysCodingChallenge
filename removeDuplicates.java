f570b81c8ad3383a5aade9b32ff8462f13b52aac
DeviPriyanka611 <pulagamdevipriyanka32@gmail.com>
2025-08-02 22:25:46 +0530

    Add files via upload

---
+++ removeDuplicates.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int counter = 1;
+        for (int i = 1; i < nums.length; i++) {
+        if (nums[i] != nums[i - 1]) {
+            nums[counter] = nums[i];
+            counter++;
+    }
+}
+return counter;
+
+    }
+    public static void main(String args[])
+    {
+        Solution obj=new Solution();
+        obj.removeDuplicates(new int[]{1,1,2});
+    }
+}
